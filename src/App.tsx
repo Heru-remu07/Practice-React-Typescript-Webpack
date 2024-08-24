897ae4eb101d27cf8d09ae2da0cff2f6c84a910e
heru-remu07 <lhemmartinez@yahoo.com>
2024-08-25 02:32:16 +0800

    adding webpack environment files

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import IMAGE from './images/image1.png'
 export const App = () => {
     return <>
         <h1>Practice Creating a React Typescript Webpack Project from Scratch</h1>
+        <p>Environment: {process.env.NODE_ENV}</p>
+        <p>Project Name: {process.env.projectName}</p>
         <img src={IMAGE} alt="react image" width="600"/>
     </> 
     
